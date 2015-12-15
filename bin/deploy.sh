078bb50c8d963a8e50a3f55f375bcd73910a3646
Phillip Goldfarb <goldfarbphillip@gmail.com>
2015-12-15 04:23:11 +0000

    Added deployment script

---
+++ bin/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd `git rev-parse --show-toplevel`
+
+./bin/minify.js ./www/htdocs/js/main.js ./www/htdocs/css/main.css ./www/htdocs/js/clip.js
+
+for file in aws.json aws_config.hh jump_config.hh; do
+
+    if [[ ! -f "./www/config/${file}" ]] then
+        cp "./www/config/${file}.example" "./www/config/${file}"
+    fi
+done
