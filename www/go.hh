0a3e45537159fcf927b540f809f596526d04c7e9
Phillip Goldfarb <goldfarbphillip@gmail.com>
2015-12-15 03:49:41 +0000

    Removed logging

--- www/go.hh
+++ www/go.hh
@@ -54,24 +54,6 @@ function g_main(string $uri): void {
         header('Location:'.$item['url']['S']);
       }
 
-      if (jump_config::DO_LOG) {
-        $dyclient->updateItem(
-          array(
-            'TableName' => aws_config::LINK_TABLE,
-            'Key' => array(
-              'Object ID' => array('S' => $item['Object ID']['S']),
-              'Checksum' => array('S' => $item['Checksum']['S']),
-            ),
-            'AttributeUpdates' => array(
-              'hits' => array(
-                'Action' => 'ADD',
-                'Value' => array('N' => '1'),
-              ),
-            ),
-          ),
-        );
-      }
-
       if ($item['clicks']['N'] > 0) {
         $dyclient->updateItem(
           array(
